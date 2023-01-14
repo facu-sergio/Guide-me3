7b9573f6b2434b6cc90bdf5e05806857cde18ad8
facu <facu11ja@gmail.com>
2023-01-14 20:54:38 -0300

    db

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ dotenv.config({path:'./.env'});
 // create the connection to database
 const connectionSync = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
