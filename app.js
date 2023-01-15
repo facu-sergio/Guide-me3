dffa4bf607f88dca6f3dd9eea520c9d6592cca62
facu <facu11ja@gmail.com>
2023-01-14 21:26:30 -0300

    seeeeeeeeeeex3

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ const storage = multer.diskStorage({
 app.use(express.text());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.set('views', path.join(__dirname,'/views'));
+app.set('views', path.join(__dirname,'views'));
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //sesion
